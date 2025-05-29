d5517c04a240b4e9b574dabdc74fed478a5247b0
nedelyn21 <nedelyndonato@gmail.com>
2025-05-29 06:38:57 -0700

    Add files via upload

---
+++ Array-Activity/arrayActivity.js
@@ -0,0 +1,16 @@
+//create a variable that store my Favorite Singers
+let favSingers = ["Mariah Carey", "Bretny Spears", "Ariana Grande", "Moira Dela Torre", "A1", "Celine Dion", "Jihoon", "Bruno Mars", "Kristel Fulgar"];
+// To console the first item in the array
+console.log(favSingers[0]);
+
+//Create a variable that store my Favorite Number
+let favNumbers = [12, 14, 5];
+//Create a variable that store a mix array like strings, Integer, boolean and decimal
+let mixedArr = ["Rina", 1214, true, "Llanes", 2005];
+
+//To console the item of variable mixedArr using array index notation.
+console.log(mixedArr[0]);
+console.log(mixedArr[1]);
+console.log(mixedArr[2]);
+console.log(mixedArr[3]);
+console.log(mixedArr[4]);
